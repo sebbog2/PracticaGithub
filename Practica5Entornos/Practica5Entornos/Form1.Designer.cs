2d6d553c3fbefaf8aa97a171a0230162f1995774
sebbog2 <145073908+sebbog2@users.noreply.github.com>
2024-04-08 18:37:24 +0200

    segundo commit

--- Practica5Entornos/Practica5Entornos/Form1.Designer.cs
+++ Practica5Entornos/Practica5Entornos/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(47, 179);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(73, 19);
+            checkBox1.TabIndex = 1;
+            checkBox1.Text = "Urgente?";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(321, 188);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 54);
+            button1.TabIndex = 2;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(47, 213);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Coste:";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(106, 210);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(47, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Texto";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(47, 42);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(383, 131);
+            textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(494, 348);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private CheckBox checkBox1;
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
     }
 }
--- Practica5Entornos/Practica5Entornos/Form1.cs
+++ Practica5Entornos/Practica5Entornos/Form1.cs
@@ -2,9 +2,71 @@ namespace Practica5Entornos
 {
     public partial class Form1 : Form
     {
+        private GroupBox groupBox1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isChecked = checkBox1.Checked;
+
+            // Obtiene el texto ingresado en el TextBox
+            string texto = textBox1.Text;
+
+            try
+            {
+                // Intenta acceder al panel1 dentro de groupBox1
+                if (groupBox1.Controls["panel1"] is Panel panel && panel.Controls["textBox2"] is TextBox textBox2)
+                {
+                    // Intenta convertir el texto de textBox2 en un número
+                    if (double.TryParse(textBox2.Text, out double costo))
+                    {
+                        // Realiza el cálculo si el valor ingresado es un número válido
+                        double resultado = isChecked ? costo * 1.1 : costo;
+                        MessageBox.Show($"El resultado es: {resultado}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Por favor ingrese un costo válido.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo acceder al control textBox2 dentro de groupBox1.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ha ocurrido un error: {ex.Message}");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
